b92577a5f5d861001f1fd2411a917f6102ff9861
Kovid63 <kovid63@gmail.com>
2024-09-18 21:14:40 +0530

    added test get route

--- server.js
+++ server.js
@@ -106,6 +106,10 @@ app.post('/scrape', checkLicense, async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
